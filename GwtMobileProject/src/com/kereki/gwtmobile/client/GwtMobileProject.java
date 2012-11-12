00ee5d197b3893e997e0b7087701fcdc15118db9
fkereki <fkereki@gmail.com>
2012-11-11 23:27:05 -0200

    Removed Window.alert(...)

--- GwtMobileProject/src/com/kereki/gwtmobile/client/GwtMobileProject.java
+++ GwtMobileProject/src/com/kereki/gwtmobile/client/GwtMobileProject.java
@@ -4,11 +4,9 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Window;
 import com.kereki.gwtmobile.client.AllEntriesForm.AllEntriesPresenter;
 
 public class GwtMobileProject implements EntryPoint, ValueChangeHandler<String> {
-
   Environment environment;
 
 
@@ -17,53 +15,10 @@ public class GwtMobileProject implements EntryPoint, ValueChangeHandler<String> 
     History.addValueChangeHandler(this);
     environment= new Environment(new Model());
     environment.launch(AllEntriesPresenter.PLACE);
-
-    // Model model= new Model();
-    //
-    // model.getListOfEntries(new SimpleCallback<ListOfEntries>() {
-    // @Override
-    // public void goBack(ListOfEntries result) {
-    // String ss= "";
-    // for (int i= 0; i < result.size(); i++) {
-    // ss+= result.get(i).date + "-" + result.get(i).title + "\n";
-    // }
-    // Window.alert(ss);
-    // }
-    // });
-
-    // diaryService.putEntry(new DiaryEntry(new Date(), "a title", "some text",
-    // 8),
-    // new AsyncCallback<Void>() {
-    //
-    // @Override
-    // public void onFailure(Throwable caught) {
-    // Window.alert("FAILURE 2!!");
-    // }
-    //
-    // @Override
-    // public void onSuccess(Void result) {
-    // Window.alert("SUCCESS 2!!");
-    // }
-    // });
-
-    // Storage stockStore= Storage.getLocalStorageIfSupported();
-    // if (stockStore == null) {
-    // Window.alert("No funciona stockstore...");
-    // } else {
-    // if (stockStore.getItem("myprefix." + "FK") == null) {
-    // stockStore.setItem("myprefix.FK", "FEDERICO KEREKI");
-    // Window.alert("no estaba, lo puse..." + stockStore.getLength());
-    // } else {
-    // Window.alert("Me encontré... " + stockStore.getItem("myprefix.FK"));
-    // // stockStore.clear();
-    // }
-    // }
-
   }
 
   @Override
   public void onValueChange(ValueChangeEvent<String> event) {
-    Window.alert("onvaluechange " + event.getValue());
     environment.launch(event.getValue());
   }
 }
