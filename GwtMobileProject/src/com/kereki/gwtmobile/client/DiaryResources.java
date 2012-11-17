e5ca0e05e2eff3101e97937cbce771309dca9d4a
fkereki <fkereki@gmail.com>
2012-11-16 22:47:09 -0200

    Dynamic loading of CSS through a resource bundle

---
+++ GwtMobileProject/src/com/kereki/gwtmobile/client/DiaryResources.java
@@ -0,0 +1,10 @@
+package com.kereki.gwtmobile.client;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface DiaryResources extends ClientBundle {
+
+  @Source("GwtMobileProject.css")
+  public CssResource css();
+}
--- GwtMobileProject/src/com/kereki/gwtmobile/client/GwtMobileProject.java
+++ GwtMobileProject/src/com/kereki/gwtmobile/client/GwtMobileProject.java
@@ -15,8 +15,10 @@ public class GwtMobileProject implements EntryPoint, ValueChangeHandler<String> 
   public void onModuleLoad() {
     final Model model= new Model();
     final ViewFactory viewFactory= GWT.create(ViewFactory.class);
+    final DiaryResources diaryResources= GWT.create(DiaryResources.class);
 
     History.addValueChangeHandler(this);
+    diaryResources.css().ensureInjected();
     environment= new Environment(model, viewFactory);
     environment.launch(AllEntriesPresenter.PLACE);
     environment.model.putPendingEntries();
